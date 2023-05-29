6758ebfd0b6c488d55831130b71ce6b705e058d3
@k$hay <akshaynaik5787@gmail.com>
2023-05-29 20:09:08 +0530

    checjking sofn

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -22,48 +22,48 @@ class ChatEngine{
                 chatroom: 'codeial'
             });
 
-            // self.socket.on('user_joined', function(data) {
-            //     console.log('a user joined....!', data);
-            // }); 
+            self.socket.on('user_joined', function(data) {
+                console.log('a user joined....!', data);
+            }); 
 
-            // // send a message on clicking the send message button
-            // $('#send-message').click(function(){
-            //     let msg = $('#chat-message-input').val();
+            // send a message on clicking the send message button
+            $('#send-message').click(function(){
+                let msg = $('#chat-message-input').val();
 
-            //     if (msg != ''){
-            //         self.socket.emit('send_message', {
-            //             message: msg,
-            //             user_email: self.userEmail,
-            //             chatroom: 'codeial'
-            //         });
-            //     }
-            // });
+                if (msg != ''){
+                    self.socket.emit('send_message', {
+                        message: msg,
+                        user_email: self.userEmail,
+                        chatroom: 'codeial'
+                    });
+                }
+            });
 
 
-            // self.socket.on('receive_message', function(data){
-            //     console.log('message received', data.message);
+            self.socket.on('receive_message', function(data){
+                console.log('message received', data.message);
     
     
-            //     let newMessage = $('<li>');
+                let newMessage = $('<li>');
     
-            //     let messageType = 'other-message';
+                let messageType = 'other-message';
     
-            //     if (data.user_email == self.userEmail){
-            //         messageType = 'self-message';
-            //     }
+                if (data.user_email == self.userEmail){
+                    messageType = 'self-message';
+                }
     
-            //     newMessage.append($('<span>', {
-            //         'html': data.message
-            //     }));
+                newMessage.append($('<span>', {
+                    'html': data.message
+                }));
     
-            //     newMessage.append($('<sub>', {
-            //         'html': data.user_email
-            //     }));
+                newMessage.append($('<sub>', {
+                    'html': data.user_email
+                }));
     
-            //     newMessage.addClass(messageType);
+                newMessage.addClass(messageType);
     
-            //     $('#chat-messages-list').append(newMessage);
-            // });
+                $('#chat-messages-list').append(newMessage);
+            });
 
         });
     }
